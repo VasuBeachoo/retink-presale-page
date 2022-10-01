45876255d516062637f5bc127bea7dbbcfa7a638
VasuBeachoo <vasubeachoo@gmail.com>
2022-10-01 14:59:03 -0400

    Create footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,64 @@
 import styled from "styled-components";
+import Logo from "./Logo";
+import {
+  FacebookIcon,
+  InstagramIcon,
+  LinkedInIcon,
+  PinterestIcon,
+  TwitterIcon,
+  YouTubeIcon,
+} from "./SocialIcons";
+
+export const FooterCopyrightLink = styled.p``;
+
+export const FooterCopyright = styled.p``;
+
+export const FooterCopyrightBox = styled.div``;
+
+export const FooterSocialIconsBox = styled.div``;
+
+export const FooterConnectHeading = styled.h3``;
+
+export const FooterConnectBox = styled.div``;
+
+export const FooterInfoText = styled.p``;
+
+export const FooterInfoBox = styled.div``;
+
+export const FooterContainer = styled.div``;
 
 export const FooterBox = styled.footer``;
 
 const Footer = ({ className }) => {
-  return <FooterBox className={className}></FooterBox>;
+  return (
+    <FooterBox className={className}>
+      <FooterContainer>
+        <Logo />
+        <FooterInfoBox>
+          <FooterInfoText>Product by Retink Media UG</FooterInfoText>
+          <FooterInfoText>Bonn, Germany</FooterInfoText>
+          <FooterInfoText>Get Early Access</FooterInfoText>
+          <FooterInfoText>Provide Feedback</FooterInfoText>
+        </FooterInfoBox>
+        <FooterConnectBox>
+          <FooterConnectHeading>Connect with Us</FooterConnectHeading>
+          <FooterSocialIconsBox>
+            <FacebookIcon />
+            <TwitterIcon />
+            <InstagramIcon />
+            <LinkedInIcon />
+            <YouTubeIcon />
+            <PinterestIcon />
+          </FooterSocialIconsBox>
+        </FooterConnectBox>
+      </FooterContainer>
+      <FooterCopyrightBox>
+        <FooterCopyright>Copyright &copy; 2021 Retink</FooterCopyright>
+        <FooterCopyrightLink>Privacy Policy</FooterCopyrightLink>
+        <FooterCopyrightLink>Terms of Service</FooterCopyrightLink>
+      </FooterCopyrightBox>
+    </FooterBox>
+  );
 };
 
 export default Footer;
--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -1,1 +1,37 @@
 import styled from "styled-components";
+import iconFacebook from "../assets/facebook.png";
+import iconTwitter from "../assets/twitter.png";
+import iconInstagram from "../assets/instagram.png";
+import iconLinkedIn from "../assets/linkedin.png";
+import iconYouTube from "../assets/youtube.png";
+import iconPinterest from "../assets/pinterest.png";
+
+export const FacebookIcon = styled.img.attrs({
+  src: iconFacebook,
+  alt: "facebook-icon",
+})``;
+
+export const TwitterIcon = styled.img.attrs({
+  src: iconTwitter,
+  alt: "twitter-icon",
+})``;
+
+export const InstagramIcon = styled.img.attrs({
+  src: iconInstagram,
+  alt: "instagram-icon",
+})``;
+
+export const LinkedInIcon = styled.img.attrs({
+  src: iconLinkedIn,
+  alt: "linkedin-icon",
+})``;
+
+export const YouTubeIcon = styled.img.attrs({
+  src: iconYouTube,
+  alt: "youtube-icon",
+})``;
+
+export const PinterestIcon = styled.img.attrs({
+  src: iconPinterest,
+  alt: "pinterest-icon",
+})``;
