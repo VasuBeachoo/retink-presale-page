ab7f8241c7ab204dc2a2d479a2c1e4f685c488a4
VasuBeachoo <vasubeachoo@gmail.com>
2022-10-01 14:16:21 -0400

    Create feature panels section

--- src/components/FeaturePanel.js
+++ src/components/FeaturePanel.js
@@ -1,9 +1,21 @@
 import styled from "styled-components";
 
+export const FeaturePanelDescription = styled.p``;
+
+export const FeaturePanelHeading = styled.h3``;
+
+export const FeaturePanelIcon = styled.img``;
+
 export const FeaturePanelBox = styled.div``;
 
-const FeaturePanel = ({ className }) => {
-  return <FeaturePanelBox className={className}></FeaturePanelBox>;
+const FeaturePanel = ({ className, icon, heading, description }) => {
+  return (
+    <FeaturePanelBox className={className}>
+      <FeaturePanelIcon src={icon.src} alt={icon.alt} />
+      <FeaturePanelHeading>{heading}</FeaturePanelHeading>
+      <FeaturePanelDescription>{description}</FeaturePanelDescription>
+    </FeaturePanelBox>
+  );
 };
 
 export default FeaturePanel;
--- src/components/presale/FeaturePanels.js
+++ src/components/presale/FeaturePanels.js
@@ -1,9 +1,77 @@
 import styled from "styled-components";
+import FeaturePanel from "../FeaturePanel";
+import imgAI from "../../assets/artificial-intelligence.png";
+import imgBrandIdentity from "../../assets/brand-identity.png";
+import imgInformed from "../../assets/informed.png";
+import imgReview from "../../assets/review.png";
+import imgMonitor from "../../assets/monitor.png";
 
 export const FeaturePanelsBox = styled.section``;
 
 const FeaturePanels = ({ className }) => {
-  return <FeaturePanelsBox className={className}></FeaturePanelsBox>;
+  const featurePanels = [
+    {
+      icon: {
+        src: imgAI,
+        alt: "artificial-intelligence-image",
+      },
+      heading: "Make AI-assisted Content Choices",
+      description:
+        "Access and Order from an extensive catalogue in any language and on your budget's terms. If unsure, our AI guides you to the best.",
+    },
+    {
+      icon: {
+        src: imgBrandIdentity,
+        alt: "brand-identity-image",
+      },
+      heading: "Upload and Maintain your Brand Identity",
+      description:
+        "Add your preferences and brand assets to ensure the contents is consistent with your brand identity. No brand asset? We can make for you!",
+    },
+    {
+      icon: {
+        src: imgInformed,
+        alt: "informed-image",
+      },
+      heading: "Be informed as we create",
+      description:
+        "Your details are translated and sent to the AI, FP, or both (per your choice) as you relax and receive continuous updates.",
+    },
+    {
+      icon: {
+        src: imgReview,
+        alt: "review-image",
+      },
+      heading: "Receive and Review",
+      description:
+        "The finished content is delivered and you can make corrections that we will effect to satisfy your requirements.",
+    },
+    {
+      icon: {
+        src: imgMonitor,
+        alt: "monitor-image",
+      },
+      heading: "Publish and Monitor your contents' progress",
+      description:
+        "The finished content is delivered and you can make corrections that we will effect to satisfy your requirements.",
+    },
+  ];
+
+  const displayFeaturePanels = (featurePanels) => {
+    return featurePanels.map((panel) => (
+      <FeaturePanel
+        icon={panel.icon}
+        heading={panel.heading}
+        description={panel.description}
+      />
+    ));
+  };
+
+  return (
+    <FeaturePanelsBox className={className}>
+      {displayFeaturePanels(featurePanels)}
+    </FeaturePanelsBox>
+  );
 };
 
 export default FeaturePanels;
