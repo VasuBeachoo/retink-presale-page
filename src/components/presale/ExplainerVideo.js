8b3cd4078a2eb04bbda341f05bdc9d4d0888f98e
VasuBeachoo <vasubeachoo@gmail.com>
2022-10-01 13:50:18 -0400

    Create explainer video section

--- src/components/presale/ExplainerVideo.js
+++ src/components/presale/ExplainerVideo.js
@@ -1,9 +1,23 @@
 import styled from "styled-components";
+import imgVideo from "../../assets/explainer-video.png";
+
+export const ExplainerVideoImg = styled.img``;
+
+export const ExplainerVideoImgBox = styled.div``;
+
+export const ExplainerVideoHeading = styled.h2``;
 
 export const ExplainerVideoBox = styled.section``;
 
 const ExplainerVideo = ({ className }) => {
-  return <ExplainerVideoBox className={className}></ExplainerVideoBox>;
+  return (
+    <ExplainerVideoBox className={className}>
+      <ExplainerVideoHeading>EXPLAINER VIDEO</ExplainerVideoHeading>
+      <ExplainerVideoImgBox>
+        <ExplainerVideoImg src={imgVideo} alt="explainer-video-screenshot" />
+      </ExplainerVideoImgBox>
+    </ExplainerVideoBox>
+  );
 };
 
 export default ExplainerVideo;
